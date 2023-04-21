df5baa7abe3e6447c4dee9fd6b1228aef0695111
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-04-20 21:02:24 -0400

    commit tree aa0cf204e42267331e9f545cf7e64f0c629fba9d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,33 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    goods = Bakery.all
+    # send them back as a JSON array
+    goods.to_json
+  end
+
+  get '/bakeries/:id' do
+    # get all the bakeries from the database
+    goods = Bakery.find(params[:id])
+    # send them back as a JSON array
+    goods.to_json(include: :baked_goods)
+  end
+  
+  get '/baked_goods/by_price' do
+    # get all the bakeries from the database
+    goods = BakedGood.order("price DESC")
+    # send them back as a JSON array
+    goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    # get all the bakeries from the database
+    goods = BakedGood.order("price DESC").first
+    # send them back as a JSON array
+    goods.to_json
+  end
 
 end
